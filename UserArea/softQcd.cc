201bb24986ab2dcc28ca5960fae16c9ad4cadd74
Vladimir <vladimir.cherepanov@cern.ch>
2019-04-12 18:53:45 +0200

    small fix to softQCD

--- UserArea/softQcd.cc
+++ UserArea/softQcd.cc
@@ -1,13 +1,10 @@
-// File: tree.cc
-// This is a simple test program.
-// Modified by Rene Brun and Axcel Naumann to put the Pythia::event 
-// into a TTree.
-// Copyright (C) 2013 Torbjorn Sjostrand
+// File: softQcd.cc
+// Vladimir Cherepanov
 
 // Header file to access Pythia 8 program elements.
 #include "Pythia.h"
 
-// ROOT, for saving Pythia events as trees in a file.
+
 #include "TTree.h"
 #include "TFile.h"
 
@@ -15,33 +12,19 @@ using namespace Pythia8;
 
 int main(int argc,char **argv) {
 
-  // Create Pythia instance and set it up to generate hard QCD processes
-  // above pTHat = 20 GeV for pp collisions at 14 TeV.
-  //  Pythia pythia;
-  //  pythia.readString("HardQCD:all = on");
-  //  pythia.readString("PhaseSpace:pTHatMin = 20.");
-  //  pythia.readString("Beams:eCM = 14000.");
-  //  pythia.init();
-
-  // Set up generation.
-  Pythia pythia; // Declare Pythia object
+  Pythia pythia; 
   pythia.readFile(argv[1]);
-  pythia.init(); // Initialize; incoming pp beams is default.
-  // Generate event(s).
+  pythia.init(); 
 
   int nEvent = pythia.mode("Main:numberOfEvents");
 
-
   double tau_p(0.);
-  // Set up the ROOT TFile and TTree.
   TFile *file = TFile::Open("output.root","recreate");
   TTree * t = new TTree("T","pythia_tree");
   t->Branch("tau_p",&tau_p);
-  //  Event *event = &pythia.event;
-  //  TTree *T = new TTree("T","ev1 Tree");
-  //  T->Branch("event",&event);
 
- // Begin event loop. Generate event; skip if generation aborted.
+
+
   for(int iEvent = 0; iEvent < nEvent; ++iEvent){
     if (!pythia.next()) continue;
 
@@ -54,20 +37,12 @@ int main(int argc,char **argv) {
     tau_p=pythia.event[iTau].pT();
 
     t->Fill();
-    // Fill the pythia event into the TTree.  
-    // Warning: the files will rapidly become large if all events 
-    // are saved. In some cases it may be convenient to do some 
-    // processing of events and only save those that appear 
-    // interesting for future analyses.
-    //    T->Fill();
-
   // End event loop.
   }
 
   // Statistics on event generation.
   pythia.stat();
 
-  //  Write tree.
   //  T->Print();
   t->Write();
   delete file;
